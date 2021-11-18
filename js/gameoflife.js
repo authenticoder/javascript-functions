1ea8456dc4865188f915c5cecf59c43f25772d8e
authenticoder <authenticoderio@gmail.com>
2021-11-18 14:41:57 +0300

    c7

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -45,7 +45,9 @@ const printCells = (state) => {
 };
 
 const getNeighborsOf = ([x, y]) => {
-  [[x-1, y-1], [x, y-1], [x+1, y-1], [x-1, y], [x+1, y], [x-1, y+1], [x, y+1], [x+1, y+1]];
+  [x-1, y-1], [x, y-1], [x+1, y-1], 
+  [x-1, y],             [x+1, y], 
+  [x-1, y+1], [x, y+1], [x+1, y+1];
 };
 
 const getLivingNeighbors = (cell, state) => {};
